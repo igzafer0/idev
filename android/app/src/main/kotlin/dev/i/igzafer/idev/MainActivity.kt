f849d5286cd131677fe75711e375fe192811cdf0
Zafer <zaferkurumsal@gmail.com>
2023-02-16 22:22:31 +0300

    first commit

---
+++ android/app/src/main/kotlin/dev/i/igzafer/idev/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.i.igzafer.idev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const IdevRun());
+}
+
+class IdevRun extends StatelessWidget {
+  const IdevRun({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.black);
+  }
+}
+//IDEV ---- START   
